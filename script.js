b36f7d39bb23136f498da768cbf3b2b91dfa4f2b
Konrad Milewski <konradmilewski2000@interia.eu>
2021-10-05 17:09:25 +0200

    initial comit

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// STICKY NAV
+
+const header = document.querySelector("header");
+
+const navigation = document.querySelector(".nav");
+
+const headerObsCallback = function (entries, observer) {
+  const entry = entries[0];
+  console.log(entry);
+
+  //   logika
+  if (!entry.isIntersecting) {
+    navigation.classList.add("sticky");
+  } else {
+    navigation.classList.remove("sticky");
+  }
+};
+
+const headerObsOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: "-76.13px",
+};
+
+const headerObserver = new IntersectionObserver(
+  headerObsCallback,
+  headerObsOptions
+);
+
+headerObserver.observe(header);
+
+// Przyciski nawigacji
+
+document.querySelector(".nav-links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  console.log(e.target);
+
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+
+    console.log(id);
+
+    document.querySelector(`.${id}`).scrollIntoView({ behavior: "smooth" });
+  }
+});
