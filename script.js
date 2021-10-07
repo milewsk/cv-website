781fc4999a1a07100300af23f39c35e24a3f00c9
Konrad Milewski <konradmilewski2000@interia.eu>
2021-10-07 20:50:26 +0200

    Test dla szerokości viewport

--- script.js
+++ script.js
@@ -119,6 +119,9 @@ document.documentElement.style.setProperty("--vh", `${vh}px`);
 window.addEventListener("resize", () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  let vw = window.innerWidth * 0.01;
+  document.documentElement.style.setProperty("--vw", `${vw}px`);
 });
 
 // button Scroll up
